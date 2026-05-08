d689f89ad980be96c35793036fe46245b0f41395
xoxoisme <kxoxo3333@gmail.com>
2026-05-08 17:38:32 +0900

    init: 초기 설정(의존성 및 자바 설정

---
+++ src/main/java/com/xoxoisme/mindkeyword/MindkeywordApplication.java
@@ -0,0 +1,13 @@
+package com.xoxoisme.mindkeyword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindkeywordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindkeywordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xoxoisme/mindkeyword/MindkeywordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xoxoisme.mindkeyword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindkeywordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
