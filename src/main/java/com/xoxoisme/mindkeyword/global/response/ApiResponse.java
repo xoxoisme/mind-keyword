9aebd59f198926f50ea23b25fa45f23ae25f8398
xoxoisme <kxoxo3333@gmail.com>
2026-05-09 20:27:37 +0900

    feat: 응답 공통화 추가

---
+++ src/main/java/com/xoxoisme/mindkeyword/global/response/ApiResponse.java
@@ -0,0 +1,25 @@
+package com.xoxoisme.mindkeyword.global.response;
+
+import lombok.Getter;
+
+@Getter
+public class ApiResponse<T> {
+
+    private final boolean success;
+    private final T data;
+
+    private ApiResponse(boolean success, T data) {
+        this.success = success;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(true, data);
+    }
+
+    public static ApiResponse<Void> ok() {
+        return new ApiResponse<>(true, null);
+    }
+}
+
+
