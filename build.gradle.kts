042fb28cec0f146c4a10f7b87818c80fd447dd84
xoxoisme <kxoxo3333@gmail.com>
2026-05-10 23:28:43 +0900

    feat: jwt 토큰 생성, 검증, 유저정보 추출 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,9 +35,9 @@ dependencies {
 	testCompileOnly("org.projectlombok:lombok")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testAnnotationProcessor("org.projectlombok:lombok")
-	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
-	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
 
 tasks.withType<Test> {
