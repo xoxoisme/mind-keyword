52d84a479571684ee52ec46ca05a691cde138d61
xoxoisme <kxoxo3333@gmail.com>
2026-05-12 16:52:12 +0900

    feat: mindmap entity 추가

---
+++ src/main/java/com/xoxoisme/mindkeyword/domain/mindmap/entity/MindMap.java
@@ -0,0 +1,22 @@
+package com.xoxoisme.mindkeyword.domain.mindmap.entity;
+
+import com.xoxoisme.mindkeyword.domain.user.entity.User;
+import com.xoxoisme.mindkeyword.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "mind_map")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MindMap extends BaseTimeEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+}
