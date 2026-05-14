eb2f8c7624b4099ed41133d3d70133b2383e8a74
xoxoisme <kxoxo3333@gmail.com>
2026-05-14 17:08:45 +0900

    refactor: 어노테이션 제거

--- src/main/java/com/xoxoisme/mindkeyword/domain/node/repository/NodeRepository.java
+++ src/main/java/com/xoxoisme/mindkeyword/domain/node/repository/NodeRepository.java
@@ -1,13 +1,8 @@
 package com.xoxoisme.mindkeyword.domain.node.repository;
 
 import com.xoxoisme.mindkeyword.domain.node.entity.Node;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
+
 public interface NodeRepository extends JpaRepository<Node, Long> {
 }
