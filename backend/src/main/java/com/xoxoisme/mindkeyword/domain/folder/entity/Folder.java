271adbcd70535a18c5665f0b60f6a41365f93a39
xoxoisme <kxoxo3333@gmail.com>
2026-05-17 14:33:24 +0900

    feat: 폴더 엔티티 추가

---
+++ backend/src/main/java/com/xoxoisme/mindkeyword/domain/folder/entity/Folder.java
@@ -0,0 +1,22 @@
+package com.xoxoisme.mindkeyword.domain.folder.entity;
+
+import com.xoxoisme.mindkeyword.domain.user.entity.User;
+import com.xoxoisme.mindkeyword.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "folder")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Folder extends BaseTimeEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+}
