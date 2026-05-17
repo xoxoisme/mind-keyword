ad7f85dab6c35d3bfbdebce18171fc32e4399ede
xoxoisme <kxoxo3333@gmail.com>
2026-05-17 14:02:05 +0900

    refactor: 구조 변경

---
+++ backend/src/main/java/com/xoxoisme/mindkeyword/domain/folder/repository/FolderRepository.java
@@ -0,0 +1,7 @@
+package com.xoxoisme.mindkeyword.domain.folder.repository;
+
+import com.xoxoisme.mindkeyword.domain.folder.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+}
